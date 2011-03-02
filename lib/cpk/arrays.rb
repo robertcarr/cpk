f4aaf42fea5ca19b5a3b88e5d1249b6536ff48d2
Robert Carr <robert@rightscale.com>
2011-03-02 19:01:40 +0000

    added deployment name for arrays vs. href

--- lib/cpk/arrays.rb
+++ lib/cpk/arrays.rb
@@ -1,13 +1,18 @@
 module Cpk
 	def self.create_arrays(arrays)
 	fix = Array.new
-	fix = %w{ min_count max_count resize_up_by resize_down_by resize_calm_time }
+	fix = %w{ min_count max_count resize_up_by resize_down_by resize_calm_time decision_threshold }
 	h = Hash.new
+
 		arrays.each do |array|
-		fix.each do |k|
-			h[k] = array[k]
-		end
+			array['deployment_href'] = deployment_href_by_name(array['deployment_href'])
+
+			fix.each do |k|
+				h[k] = array[k]
+			end
 			array['elasticity'] = h
+			Ec2ServerArray.create(array) unless @debug
+
 		end
 	end
 end
