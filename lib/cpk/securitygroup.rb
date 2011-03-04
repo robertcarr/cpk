0112dca80e3748718d190095b6d3768e2a29130d
Robert Carr <robert@rightscale.com>
2011-03-04 03:09:02 +0000

    added --destroy to security groups

--- lib/cpk/securitygroup.rb
+++ lib/cpk/securitygroup.rb
@@ -1,8 +1,13 @@
 module Cpk
 	def self.create_securitygroups(groups)
 		groups.each do |group|
+		if @options[:destroy]
+			g = Ec2SecurityGroup.find(:first) { |x| x.aws_group_name =~ /#{group["aws_group_name"]}/ }
+			g.destroy unless @debug
+		else
 			Ec2SecurityGroup.create(group) unless @debug
-			puts group.inspect if @debug
+		end
+		puts group.inspect if @debug
 		end
 	end
 end
