1773ec30b0a3cf24433ecee3fbf46f780b77e936
Robert Carr <robert@rightscale.com>
2011-03-04 03:42:29 +0000

    late commit

--- lib/cpk/credentials.rb
+++ lib/cpk/credentials.rb
@@ -3,9 +3,14 @@ module Cpk
 		creds.each do |cred|
 		if @options[:destroy]
 			c = Credential.find(:first) { |x| x.name =~ /#{cred["name"]}/ }
-			c.destroy unless @debug
+			c.destroy unless c.nil? || @debug
 		else
+			begin
 			Credential.create(cred) unless @debug
+			rescue
+				puts "Error creating #{cred["name"]}"
+			end
+
 		end
 			puts cred.inspect if @debug
 		end
