6d2d1f97c8bb48b66cdbce9ea60edc9c9b100214
Robert Carr <robert@rightscale.com>
2011-03-04 05:02:35 +0000

    Commit before bed

--- lib/cpk/deployments.rb
+++ lib/cpk/deployments.rb
@@ -1,9 +1,15 @@
+# TODO: FIX deployment['description'] to not double escape \n
 module Cpk
 	def self.create_deployments(deps)
+	puts "go"
 		deps.each do |dep|
-# TODO: FIX deployment['description'] to not double escape \n
-			Deployment.create(dep) unless @debug
-			puts dep.inspect if @debug
+			if @options[:destroy]
+				d = Deployment.find(:all) { |x| x.nickname =~ /#{dep['nickname']}/ }
+				d.destroy unless @debug
+			else
+				Deployment.create(dep) unless @debug
+			end
+				puts dep.inspect if @debug
 		end
 	end
 end
