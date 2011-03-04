2cda45266ae269b915e86fc472a06923e92b37e2
Robert Carr <robert@rightscale.com>
2011-03-04 02:57:03 +0000

    added --destroy OBJECT for credentials to delete them

--- lib/cpk/credentials.rb
+++ lib/cpk/credentials.rb
@@ -1,7 +1,12 @@
 module Cpk
 	def self.create_credentials(creds)
 		creds.each do |cred|
+		if @options[:destroy]
+			c = Credential.find(:first) { |x| x.name =~ /#{cred["name"]}/ }
+			c.destroy unless @debug
+		else
 			Credential.create(cred) unless @debug
+		end
 			puts cred.inspect if @debug
 		end
 	end
