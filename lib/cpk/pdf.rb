8c4a85576748b66b058eadab51cacf465414b375
Robert Carr <robert@rightscale.com>
2011-03-14 17:25:11 +0000

    started --pdf support

--- lib/cpk/pdf.rb
+++ lib/cpk/pdf.rb
@@ -3,18 +3,16 @@ require 'prawn'
 
 module Cpk
   def self.create_pdf
-headers = ""
-data = ""
-    servers = extract("servers")
-    servers.each do |row|
-x=0
-      row.each do |item, val|
-      headers << "#{item}\t" unless x
-      data << "#{val}\t"
-      end
-x += 1
-    end
-puts headers
+  pdf = Prawn::Document.new
+  servers = extract("servers")
+  servers.each do |x|
+  pdf.text(x['nickname'])
+  pdf.text(x['server_template_href'])
+  pdf.text(x['instance_type'])
+  end
+
+  pdf.text("tester")
+  pdf.render_file("doc.pdf")
   end
 end
 
